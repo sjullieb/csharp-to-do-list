6408953c787f18ef1bb7dc57c7533f02bc57ba05
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-02-12 23:37:37 -0800

    add test cases

--- ToDoList/Model/Item.cs
+++ ToDoList/Model/Item.cs
@@ -1,9 +1,35 @@
+using System.Collections.Generic;
 namespace ToDoList.Models
 {
   public class Item
   {
+    private string _description;
+    private static List<Item> _instances = new List<Item> {};
 
-    // properties, methods, etc. will go here.
+    public Item (string description)
+    {
+      _description = description;
+      _instances.Add(this);
+    }
 
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }    
   }
 }
